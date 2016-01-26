6ee641197ba8daa20942c0f7da7123a4e8a6aa91
furesoft <chris.anders@outlook.de>
2016-01-26 21:43:21 +0100

    updated the nuget package

---
+++ XmlComponent/Package/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+
+namespace XmlComponent.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ComponentTest()
+        {
+            var dom = new XmlDocument();
+
+            dom.LoadXml("<test><hello title='true'>world</hello></test>");
+            dom.CreateComponent("<hello><p title='{title}'>{title}</p></hello>");
+
+            dom.TransformComponents();
+        }
+
+        [TestMethod]
+        public void IncludeTest()
+        {
+            var dom = new XmlDocument();
+            dom.LoadXml("<test><include src='include.xml' type='text/xml' /></test>");
+
+            dom.TransformIncludes();
+        }
+
+        [TestMethod]
+        public void TestBindingQuery()
+        {
+            var q = new Binding();
+            q.Query.Parse("{[chILdReN]}");
+
+            Assert.IsTrue(q.Query.Children);
+        }
+    }
+}
